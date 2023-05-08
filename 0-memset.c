55d5b64b29659b901f9d2d1e373da3a541a79791
sahar9900 <zer.sahar99@gmail.com>
2023-05-08 16:31:36 -0700

    task0

--- 0-memset.c
+++ 0-memset.c
@@ -9,11 +9,10 @@
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-int i = 0;
-for (; n > 0; i++)
+unsigned int itr;
+for (itr = 0; n > 0; itr++, n--)
 {
-s[i] = b;
-n--;
+s[itr] = b;
 }
 return (s);
 }
