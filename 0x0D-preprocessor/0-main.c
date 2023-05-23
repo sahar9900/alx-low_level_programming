edb2ae3617e1498bc075a5037bf1bf1ba9eef93e
sahar9900 <zer.sahar99@gmail.com>
2023-05-23 16:39:54 -0700

    0-object_like_macro.h

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -1,6 +1,7 @@
 #include "0-object_like_macro.h"
 #include "0-object_like_macro.h"
 #include <stdio.h>
+
 /**
 * main - check the code
 *
@@ -9,6 +10,7 @@
 int main(void)
 {
 int s;
+
 s = 98 + SIZE;
 printf("%d\n", s);
 return (0);
